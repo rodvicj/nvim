9a88c882a163ad88d21d7fdbffd0a2d940f02f16
rodvicj <rodvicj@gmail.com>
2024-02-01 20:07:35 +0800

    init

---
+++ lua/user/nvimtree.lua
@@ -0,0 +1,137 @@
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts "Up")
+  vim.keymap.set("n", "?", api.tree.toggle_help, opts "Help")
+
+  vim.keymap.set("n", "l", api.node.open.edit, opts "Help")
+  vim.keymap.set("n", "h", api.node.navigate.parent_close, opts "Help")
+  -- ["<CR>"] = { api.node.open.edit, opts "Open" },
+  -- ["v"] = { api.node.open.vertical, opts "Open: Vertical Split" },
+  -- ["C"] = { api.tree.change_root_to_node, opts "CD" },
+  -- ["<S-k>"] = { api.node.open.preview, opts "Open Preview" },
+end
+
+local M = {
+  "nvim-tree/nvim-tree.lua",
+  event = "VeryLazy",
+}
+
+function M.config()
+  local wk = require "which-key"
+  wk.register {
+    ["<leader>e"] = { "<cmd>NvimTreeToggle<CR>", "Explorer" },
+  }
+
+  local icons = require "user.icons"
+
+  require("nvim-tree").setup {
+    on_attach = my_on_attach,
+    disable_netrw = false,
+    hijack_cursor = false,
+    hijack_netrw = true,
+    hijack_unnamed_buffer_when_opening = false,
+    sync_root_with_cwd = true,
+    view = {
+      relativenumber = false,
+    },
+    renderer = {
+      add_trailing = false,
+      group_empty = false,
+      highlight_git = false, --true in lvim
+      full_name = false,
+      highlight_opened_files = "none",
+      root_folder_label = ":t",
+      indent_width = 2,
+      indent_markers = {
+        enable = false,
+        inline_arrows = true,
+        icons = {
+          corner = "└",
+          edge = "│",
+          item = "│",
+          none = " ",
+        },
+      },
+      icons = {
+        git_placement = "before",
+        padding = " ",
+        symlink_arrow = " ➛ ",
+        glyphs = {
+          default = icons.ui.Text,
+          symlink = icons.ui.FileSymlink,
+          bookmark = icons.ui.BookMark,
+          folder = {
+            arrow_closed = icons.ui.ChevronRight,
+            arrow_open = icons.ui.ChevronShortDown,
+            default = icons.ui.Folder,
+            open = icons.ui.FolderOpen,
+            empty = icons.ui.EmptyFolder,
+            empty_open = icons.ui.EmptyFolderOpen,
+            symlink = icons.ui.FolderSymlink,
+            symlink_open = icons.ui.FolderOpen,
+          },
+          git = {
+            unstaged = icons.git.FileUnstaged,
+            staged = icons.git.FileStaged,
+            unmerged = icons.git.FileUnmerged,
+            renamed = icons.git.FileRenamed,
+            untracked = icons.git.FileUntracked,
+            deleted = icons.git.FileDeleted,
+            ignored = icons.git.FileIgnored,
+          },
+        },
+      },
+      special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+      symlink_destination = true,
+    },
+    update_focused_file = {
+      enable = true,
+      debounce_delay = 15,
+      update_root = true,
+      ignore_list = {},
+    },
+
+    diagnostics = {
+      enable = true,
+      show_on_dirs = false,
+      show_on_open_dirs = true,
+      debounce_delay = 50,
+      severity = {
+        min = vim.diagnostic.severity.HINT,
+        max = vim.diagnostic.severity.ERROR,
+      },
+      icons = {
+        hint = icons.diagnostics.BoldHint,
+        info = icons.diagnostics.BoldInformation,
+        warning = icons.diagnostics.BoldWarning,
+        error = icons.diagnostics.BoldError,
+      },
+    },
+  }
+end
+
+-- function M.start_telescope(telescope_mode)
+--   local node = require("nvim-tree.lib").get_node_at_cursor()
+--   local abspath = node.link_to or node.absolute_path
+--   local is_folder = node.open ~= nil
+--   local basedir = is_folder and abspath or vim.fn.fnamemodify(abspath, ":h")
+--   require("telescope.builtin")[telescope_mode] {
+--     cwd = basedir,
+--   }
+-- end
+
+-- -- pass to setup along with your other options
+-- require("nvim-tree").setup {
+--   on_attach = my_on_attach,
+-- }
+
+return M
