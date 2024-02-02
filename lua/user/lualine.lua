dbca791e6875c29bc1fedb3091f6db7b1a75eaee
rodvicj <rodvicj@gmail.com>
2024-02-02 18:46:42 +0800

    basic lualine for now

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -6,6 +6,85 @@ local M = {
 }
 
 function M.config()
+  local icons = require "user.icons"
+  local diff = {
+    "diff",
+    colored = false,
+    symbols = { added = icons.git.LineAdded, modified = icons.git.LineModified, removed = icons.git.LineRemoved }, -- Changes the symbols used by the diff.
+  }
+
+  -- local diagnostics = {
+  --   "diagnostics",
+  --   sections = { "error", "warn" },
+  --   colored = false, -- Displays diagnostics status in color if set to true.
+  --   always_visible = true, -- Show diagnostics even if there are none.
+  -- }
+
+  local filetype = {
+    function()
+      local filetype = vim.bo.filetype
+      local upper_case_filetypes = {
+        "json",
+        "jsonc",
+        "yaml",
+        "toml",
+        "css",
+        "scss",
+        "html",
+        "xml",
+      }
+
+      if vim.tbl_contains(upper_case_filetypes, filetype) then
+        return filetype:upper()
+      end
+
+      return filetype
+    end,
+  }
+
+  local colors = {
+    bg = "#202328",
+    fg = "#bbc2cf",
+    yellow = "#ECBE7B",
+    cyan = "#008080",
+    darkblue = "#081633",
+    green = "#98be65",
+    orange = "#FF8800",
+    violet = "#a9a1e1",
+    magenta = "#c678dd",
+    purple = "#c678dd",
+    blue = "#51afef",
+    red = "#ec5f67",
+  }
+
+  local window_width_limit = 100
+
+  local conditions = {
+    buffer_not_empty = function()
+      return vim.fn.empty(vim.fn.expand "%:t") ~= 1
+    end,
+    hide_in_width = function()
+      return vim.o.columns > window_width_limit
+    end,
+    -- check_git_workspace = function()
+    --   local filepath = vim.fn.expand "%:p:h"
+    --   local gitdir = vim.fn.finddir(".git", filepath .. ";")
+    --   return gitdir and #gitdir > 0 and #gitdir < #filepath
+    -- end,
+  }
+
+  local treesitter = {
+    function()
+      return ""
+    end,
+    color = function()
+      local buf = vim.api.nvim_get_current_buf()
+      local ts = vim.treesitter.highlighter.active[buf]
+      return { fg = ts and not vim.tbl_isempty(ts) and colors.green or colors.red }
+    end,
+    cond = conditions.hide_in_width,
+  }
+
   require("lualine").setup {
     options = {
       component_separators = { left = "", right = "" },
@@ -13,15 +92,24 @@ function M.config()
       ignore_focus = { "NvimTree" },
     },
     sections = {
-      lualine_a = {},
-      lualine_b = { "branch" },
-      lualine_c = { "diagnostics" },
-      lualine_x = { "copilot", "filetype" },
-      lualine_y = { "progress" },
-      lualine_z = {},
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = {},
+      lualine_x = { treesitter, filetype },
+      lualine_y = {},
+      -- lualine_z = {},
     },
-    extensions = { "quickfix", "man", "fugitive" },
+    -- extensions = { "quickfix", "man", "fugitive", "oil" },
+    -- extensions = { "quickfix", "man", "fugitive" },
   }
 end
-
+-- lualine_x = {
+--   components.treesitter,
+--   components.encoding,
+--   components.spaces,
+--   components.lsp,
+--   components.filetype,
+-- },
+-- lualine_z = { components.scrollbar },
+-- lualine_y = { components.location },
 return M
