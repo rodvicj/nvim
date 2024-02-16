baa5bac0fa47cbec32b053d0b5fd4ee3bdb3ddfd
rodvicj <rodvicj@gmail.com>
2024-02-16 09:00:26 +0800

    add some keys for buffer and nvimtree

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -35,7 +35,32 @@ function M.config()
     -- [";"] = { "<cmd>tabnew | terminal<CR>", "Term" },
     [";"] = { "<cmd>Alpha<CR>", "Dashboard" },
     v = { "<cmd>vsplit<CR>", "Split" },
-    b = { name = "Buffers" },
+    b = {
+      name = "Buffers",
+      j = { "<cmd>BufferLinePick<cr>", "Jump" },
+      f = { "<cmd>Telescope buffers previewer=false<cr>", "Find" },
+      b = { "<cmd>BufferLineCyclePrev<cr>", "Previous" },
+      n = { "<cmd>BufferLineCycleNext<cr>", "Next" },
+      W = { "<cmd>noautocmd w<cr>", "Save without formatting (noautocmd)" },
+      -- w = { "<cmd>BufferWipeout<cr>", "Wipeout" }, -- TODO: implement this for bufferline
+      e = {
+        "<cmd>BufferLinePickClose<cr>",
+        "Pick which buffer to close",
+      },
+      h = { "<cmd>BufferLineCloseLeft<cr>", "Close all to the left" },
+      l = {
+        "<cmd>BufferLineCloseRight<cr>",
+        "Close all to the right",
+      },
+      D = {
+        "<cmd>BufferLineSortByDirectory<cr>",
+        "Sort by directory",
+      },
+      L = {
+        "<cmd>BufferLineSortByExtension<cr>",
+        "Sort by language",
+      },
+    },
     d = { name = "Debug" },
     f = { name = "Find" },
     g = { name = "Git" },
@@ -95,3 +120,30 @@ function M.config()
 end
 
 return M
+
+-- b = {
+--   name = "Buffers",
+--   j = { "<cmd>BufferLinePick<cr>", "Jump" },
+--   f = { "<cmd>Telescope buffers previewer=false<cr>", "Find" },
+--   b = { "<cmd>BufferLineCyclePrev<cr>", "Previous" },
+--   n = { "<cmd>BufferLineCycleNext<cr>", "Next" },
+--   W = { "<cmd>noautocmd w<cr>", "Save without formatting (noautocmd)" },
+--   -- w = { "<cmd>BufferWipeout<cr>", "Wipeout" }, -- TODO: implement this for bufferline
+--   e = {
+--     "<cmd>BufferLinePickClose<cr>",
+--     "Pick which buffer to close",
+--   },
+--   h = { "<cmd>BufferLineCloseLeft<cr>", "Close all to the left" },
+--   l = {
+--     "<cmd>BufferLineCloseRight<cr>",
+--     "Close all to the right",
+--   },
+--   D = {
+--     "<cmd>BufferLineSortByDirectory<cr>",
+--     "Sort by directory",
+--   },
+--   L = {
+--     "<cmd>BufferLineSortByExtension<cr>",
+--     "Sort by language",
+--   },
+-- },
