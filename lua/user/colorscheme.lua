ee80a335be74ddc2de4fbaf89136d6c340a52eef
rodvicj <rodvicj@gmail.com>
2024-05-08 10:57:00 +0800

    remove highlight in winbar when toggling vsplit

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,10 +2,16 @@ local M = {
   "folke/tokyonight.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
-
 }
 
 function M.config()
+  require("tokyonight").setup {
+    on_highlights = function(hl)
+      hl.WinBar = { link = "none" }
+      hl.WinBarNC = { link = "none" }
+    end,
+  }
+
   vim.cmd.colorscheme "tokyonight-storm"
 end
 
