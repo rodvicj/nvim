ea6625516238ca767ebcb07f6596ca5898190555
rodvicj <rodvicj@gmail.com>
2024-05-13 08:46:24 +0800

    use official rainbow-delimiters

--- init.lua
+++ init.lua
@@ -44,3 +44,4 @@ spec "user.extras.typescript-tools"
 require "user.lazy"
 -- d shift 4 -- deletes current cursor up to last char in the same line
 -- 5e875ae8d07dd204ce5b1d97a8acf70e3835b40b --jsx treesitter highlight still not messed up
+-- c30ebb17f6b98625e3db8f032c2223876bb60f99 --commit that messed up my jsx, htmldjango treesitter highlight
--- lua/user/extras/rainbow-delimiters.lua
+++ lua/user/extras/rainbow-delimiters.lua
@@ -1,5 +1,5 @@
 local M = {
-  "rodvicj/rainbow-delimiters.nvim",
+  "HiPhish/rainbow-delimiters.nvim",
   event = "VeryLazy",
 }
 
@@ -13,12 +13,8 @@ function M.config()
     },
     query = {
       [""] = "rainbow-delimiters",
-      -- removed unnecessary highlighting in rodvicj/rainbow-delimiters.nvim
-      -- javascript, tsx, astro
-      javascript = "rainbow-delimiters-react",
-      -- html = "rainbow-parens",
-      -- htmldjango = "rainbow-delimiters",
-      -- NOTE: create highlighting for htmldjango if using it heavily but first you have to work with treesitter's htmldjango first;
+      javascript = "rainbow-delimiters",
+      tsx = "rainbow-parens",
     },
     highlight = {
       "rainbowcol1",
@@ -30,3 +26,38 @@ function M.config()
 end
 
 return M
+
+
+--  -- settings for my customized rainbow-delimiters
+-- local M = {
+--   "rodvicj/rainbow-delimiters.nvim",
+--   event = "VeryLazy",
+-- }
+
+
+-- function M.config()
+--   local rainbow_delimiters = require "rainbow-delimiters"
+
+--   require("rainbow-delimiters.setup").setup {
+--     strategy = {
+--       [""] = rainbow_delimiters.strategy["global"],
+--     },
+--     query = {
+--       [""] = "rainbow-delimiters",
+--       -- removed unnecessary highlighting in rodvicj/rainbow-delimiters.nvim
+--       -- javascript, tsx, astro
+--       javascript = "rainbow-delimiters-react",
+--       -- html = "rainbow-parens",
+--       -- htmldjango = "rainbow-delimiters",
+--       -- NOTE: create highlighting for htmldjango if using it heavily but first you have to work with treesitter's htmldjango first;
+--     },
+--     highlight = {
+--       "rainbowcol1",
+--       "rainbowcol2",
+--       "rainbowcol3",
+--     },
+--     blacklist = { "html" },
+--   }
+-- end
+
+-- return M
