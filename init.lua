09cbac550e2b3627511e09191b8d465215a81f5d
rodvicj <rodvicj@gmail.com>
2024-06-28 15:43:33 +0800

    remove comments

--- init.lua
+++ init.lua
@@ -50,9 +50,3 @@ spec "user.extras.zen"
 require "user.lazy"
 -- f + char == find next char
 -- zfat - fold html tags
-
--- nvim-colorizer/colorizer/tailwind.lua
--- 118 return vim.lsp.get_active_clients { bufnr = buf, name = "tailwindcss" }
---
--- 9 18 33
--- f + char == find next char
