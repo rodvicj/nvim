883192766785ab77f87f3e898694db0b3bbbc4b3
rodvicj <rodvicj@gmail.com>
2026-02-04 11:33:58 +0800

    update live-server

--- lua/plugins/live-server.lua
+++ lua/plugins/live-server.lua
@@ -1,13 +1,22 @@
-local M = {
+-- local M = {
+--   "barrett-ruth/live-server.nvim",
+--   event = "VeryLazy",
+--   build = "npm install -g live-server",
+--   cmd = { "LiveServerStart", "LiveServerStop" },
+-- }
+
+-- return M
+
+
+
+--live server
+vim.g.live_server = {
+  args = { "--port=5555" },
+}
+
+return  {
   "barrett-ruth/live-server.nvim",
   event = "VeryLazy",
   build = "npm install -g live-server",
   cmd = { "LiveServerStart", "LiveServerStop" },
-  config = true,
 }
-
-function M.config()
-  require("live-server").setup()
-end
-
-return M
