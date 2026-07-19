03c59ad230fc7305e4ed1dca3990e1351054ebfb
rodvicj <rodvicj@gmail.com>
2026-07-19 15:18:13 +0800

    update bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -29,6 +29,8 @@ return {
     },
     options = {
       mode = "buffers",
+      sort_by = "directory",
+      move_to_end = false,
       always_show_bufferline = true,
       animation = false,
       auto_hide = false,
@@ -36,10 +38,10 @@ return {
       icon_custom_colors = true,
       icon_separator_active = "▎",
       icon_separator_inactive = "▎",
-  --     close_command = function(bufnr)
-  -- -- { "<leader>c", "<cmd>BufferKill<CR>", desc = "Close Buffer" },
-  --       BufferKill("bd", bufnr, false)
-  --     end,
+      --     close_command = function(bufnr)
+      -- -- { "<leader>c", "<cmd>BufferKill<CR>", desc = "Close Buffer" },
+      --       BufferKill("bd", bufnr, false)
+      --     end,
       icon_close_tab = "",
       icon_close_tab_modified = "●",
       offsets = { { filetype = "NvimTree", text = "File Explorer", padding = 1 } },
