214dcf1acf72468012653dbd006753f9266ccb5a
rodvicj <rodvicj@gmail.com>
2026-09-22 19:10:59 +0800

    live server transfer to forge

--- lua/plugins/live-server.lua
+++ lua/plugins/live-server.lua
@@ -1,22 +1,17 @@
--- local M = {
+vim.g.live_server = {
+  port = 5555,
+  -- browser = false,
+}
+
+-- return  {
 --   "barrett-ruth/live-server.nvim",
 --   event = "VeryLazy",
 --   build = "npm install -g live-server",
 --   cmd = { "LiveServerStart", "LiveServerStop" },
 -- }
 
--- return M
-
-
-
---live server
-vim.g.live_server = {
-  args = { "--port=5555" },
-}
-
-return  {
-  "barrett-ruth/live-server.nvim",
-  event = "VeryLazy",
-  build = "npm install -g live-server",
-  cmd = { "LiveServerStart", "LiveServerStop" },
+return {
+  url = "https://forge.barrettruth.com/barrettruth/live-server.nvim",
+  name = "live-server.nvim",
+  cmd = { "LiveServerStart", "LiveServerStop", "LiveServerToggle" },
 }
